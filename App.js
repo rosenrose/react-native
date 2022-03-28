37c7d6229bf9448a6159f88bf9471fe43364c081
rosenrose <crazytempler@gmail.com>
2022-03-28 17:52:52 +0900

    styles

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>ㅇoO0</Text>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>서산</Text>
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        // persistentScrollbar={true}
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.tempetature}>27°</Text>
+          <Text style={styles.description}>맑음</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.tempetature}>27°</Text>
+          <Text style={styles.description}>맑음</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.tempetature}>27°</Text>
+          <Text style={styles.description}>맑음</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.tempetature}>27°</Text>
+          <Text style={styles.description}>맑음</Text>
+        </View>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,12 +40,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "tomato",
   },
-  text: {
-    fontSize: 200,
-    color: "violet",
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    color: "black",
+    fontSize: 68,
+    fontWeight: "bold", //["normal","bold","100","200","300","400","500","600","700","800","900"]
+  },
+  weather: {
+    // backgroundColor: "teal",
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  tempetature: {
+    fontSize: 160,
+    marginTop: 50,
+  },
+  description: {
+    fontSize: 60,
+    marginTop: -30,
   },
 });
